6c7ecff6371ceed8586c8d57030ce8300d7c528c
Kat Fries <katherynejf@gmail.com>
2018-04-11 17:12:42 -0400

    Update DownTheRabbitHole.js

--- DownTheRabbitHole.js
+++ DownTheRabbitHole.js
@@ -1,3 +1,29 @@
+//Constructors & Methods 
+
+function Dog(name, breed, sex, age){
+    this.name  = name;
+    this.breed = breed;
+    this.sex   = sex;
+    this.age   = age;
+}
+
+Dog.prototype.bark = function bark() {
+  return 'Woof!';
+}
+
+class ReturnName{
+
+  constructor(firstName, lastName){
+    this.firstName=firstName;
+    this.lastName=lastName;
+  }
+  
+  getFullName(){
+    return !this.firstName ? this.lastName : !this.lastName ? this.firstName : this.firstName+" "+this.lastName;
+  }
+  
+}
+
 // Spread
 
 function spread(func, args) {
